e36932b272e4a51cd8b1689ee0c5ce135e7b745b
pragathibaskar <47776430+pragathibaskar@users.noreply.github.com>
2019-09-22 21:15:06 +0530

    Update CertServiceApplication.java

--- src/main/java/com/cg/certService/CertServiceApplication.java
+++ src/main/java/com/cg/certService/CertServiceApplication.java
@@ -4,9 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableDiscoveryClient
 @ComponentScan("com.cg.certService.*")
 public class CertServiceApplication {
 
