0a65457cb89ab299344646db3215926cb379ccd0
pragathibaskar <47776430+pragathibaskar@users.noreply.github.com>
2019-09-25 15:24:03 +0530

    Create WebConfiguartion.java

---
+++ src/main/java/com/cg/certService/WebConfiguartion.java
@@ -0,0 +1,16 @@
+
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+	public void addCorsMapping(CorsRegistry register) {
+		register
+			.addMapping("/**")
+			.allowedMethods("*")
+			.allowedOrigins("*");
+	}
+}
